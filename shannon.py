395b45e758f9f2e8a13cdb7da0df33f7e56a7efe
shhong@mint <shhong@oist.jp>
2010-07-18 17:00:10 +0900

    Bug fixes; bootstrap for removing sampling biases.

--- shannon.py
+++ shannon.py
@@ -2,38 +2,67 @@ from numpy import zeros, log
 
 def plogp(p, epsilon):
   x = zeros(p.shape)
-  q = p[p>=epsilon]
-  x[p>=epsilon] = q * log(q)
+  ind = (p>=epsilon)
+  q = p[ind]
+  x[ind] = q * log(q)
   return x
 
-def count1(r):
+def count1(u):
+  r = u-u.min()
   N = int(r.max())
   p = zeros(N+1)
   
   for i in r:
-    p[i] = p[i] + 1
-  return p
+    j = int(float(i))
+    p[j] = p[j] + 1
+  return (p, 1.0)
 
-def count2(r1, r2):
-  L = r1.size
+def count2(u1, u2):
+  r1, r2 = u1-u1.min(), u2-u2.min()
   N1, N2 = int(r1.max()), int(r2.max())
-  p12 = zeros((N1+1, N2+1))
-
-  for i in xrange(L):
-    p12[r1[i],r2[i]] = p12[r1[i],r2[i]] + 1
   
-  return p12
+  p12 = zeros((N1+1, N2+1))
+  
+  for i, re1 in enumerate(r1):
+    j1 = int(re1)
+    j2 = int(r2[i])
+    p12[j1,j2] = p12[j1,j2] + 1
+  
+  return (p12, 1.0)
   
 def entropy1(r):
   L = r.size
-  p = count1(r)
+  p, delta = count1(r)
   p = p/L
   epsilon = 1.0/L
   return -plogp(p, epsilon).sum()
 
-def entropy2(r1, r2):
+def entropy2(*r):
+  if len(r) is 1:
+    r1, r2 = r[0]
+  else:
+    r1, r2 = r
   L = r1.size
-  p12 = count2(r1, r2)
+  p12, delta2 = count2(r1, r2)
   p12 = p12/L
   epsilon = 1.0/L
   return -plogp(p12, epsilon).sum()
+
+def bootstrap(*args, **kwargs):
+  from numpy import array, polyfit
+  default = {'factors': (1, 0.5, 0.25, 0.125), 'debug':False}
+  for k in kwargs: default[k] = kwargs[k]
+  sfunc = args[0]
+  data = args[1:]
+  N = data[0].size
+  block_sizes = array([int(N*f) for f in default['factors']])
+  if default['debug']: print block_sizes
+  ss = []
+  for n in block_sizes:
+    nblocks = N/n
+    blocked_data = [[d[i*n:(i+1)*n] for d in data] for i in xrange(nblocks)]  
+    smean = array([sfunc(*bd) for bd in blocked_data]).mean()
+    ss.append(smean)
+  if default['debug']: print ss
+  sr = polyfit(1.0/array(block_sizes), array(ss), 2)
+  return {'predict':sr[-1], 'samples':ss}
